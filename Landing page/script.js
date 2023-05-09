560846c2edc6e75fcd791e10ea72dbc37fd85f2e
Fernando Alves machado <112523029+FernandoAlvesMachado@users.noreply.github.com>
2023-05-09 17:54:15 -0400

    Add files via upload

--- Landing page/script.js
+++ Landing page/script.js
@@ -3,15 +3,34 @@ function sobre(){
 }
 
 function home(){
-    let email = document.querySelector("#iptEmail")
-    let senha = document.querySelector("#iptSenha")
-
-    if(email == "fernando@gmail.com"){
-        if (senha == 'senha123') {
             window.location.href = "index.html";
-        }
-    } else{
-        alert("Senha incorreta!")
-    }
-    
 }
+
+function portifolio(){
+    window.location.href = "portifolio.html";
+}
+
+// Rerion carrossel
+const carouselImages = document.querySelector('.carousel-images');
+const prevButton = document.querySelector('.prev-button');
+const nextButton = document.querySelector('.next-button');
+const imageSize = 700; // Tamanho das imagens do carrossel
+
+let posicao = 0; // Posição atual do carrossel
+
+nextButton.addEventListener('click', () => {
+  // Verifica se ainda há imagens para avançar
+  if (posicao < carouselImages.children.length - 1) {
+    posicao++;
+    carouselImages.style.transform = `translateX(-${posicao * imageSize}px)`;
+  }
+});
+
+prevButton.addEventListener('click', () => {
+  // Verifica se ainda há imagens para retroceder
+  if (posicao > 0) {
+    posicao--;
+    carouselImages.style.transform = `translateX(-${posicao * imageSize}px)`;
+  }
+});
+// Endregion // 
